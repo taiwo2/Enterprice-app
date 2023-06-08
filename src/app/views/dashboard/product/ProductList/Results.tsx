2d9b952da6b08daa576b0e7edd4e5a5004182517
taiwo2 <adetonat@ymail.com>
2023-06-08 23:54:54 +0100

    Add Named Import

--- src/app/views/dashboard/product/ProductList/Results.tsx
+++ src/app/views/dashboard/product/ProductList/Results.tsx
@@ -1,5 +1,44 @@
-import React from 'react';
 import { makeStyles } from '@material-ui/core';
+import React, { useState, ChangeEvent } from 'react';
+import clsx from 'clsx';
+import numeral from 'numeral';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import {
+  Image as ImageIcon,
+  Edit as EditIcon,
+  ArrowRight as ArrowRightIcon,
+  Search as SearchIcon,
+} from 'react-feather';
+import {
+  Box,
+  Button,
+  Card,
+  Checkbox,
+  InputAdornment,
+  FormControlLabel,
+  IconButton,
+  SvgIcon,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TextField,
+  makeStyles,
+} from '@material-ui/core';
+import {
+  availabilityOptions,
+  categoryOptions,
+  sortOptions,
+} from 'helpers/inputProductOptions';
+import {
+  applyFilters,
+  applyPagination,
+  TableResultsHelpers,
+  getInventoryLabel,
+} from './tableResultsHelpers';
+import { ProductType } from 'models/product-type';
 const Results = () => {
   const classes = useStyles();
   return (
