cb9a1bd6bb2560fbe758a9a9e164716337fd46f5
taiwo2 <adetonat@ymail.com>
2023-06-11 05:59:59 +0100

    Add notification Library

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import { SnackbarProvider } from 'notistack';
 import { GlobalStyle } from 'styles/global-styles';
 import { useTranslation } from 'react-i18next';
 import Routers from './routes';
@@ -11,17 +11,19 @@ export function App() {
   const { i18n } = useTranslation();
   return (
     <BrowserRouter>
-      <Helmet
-        titleTemplate="%s - React Boilerplate"
-        defaultTitle="React Boilerplate"
-        htmlAttributes={{ lang: i18n.language }}
-      >
-        <meta name="description" content="A React Boilerplate application" />
-      </Helmet>
-      <MainLayout>
-        <Routers />
-      </MainLayout>
-      <GlobalStyle />
+      <SnackbarProvider dense maxSnack={3}>
+        <Helmet
+          titleTemplate="%s - React Boilerplate"
+          defaultTitle="React Boilerplate"
+          htmlAttributes={{ lang: i18n.language }}
+        >
+          <meta name="description" content="A React Boilerplate application" />
+        </Helmet>
+        <MainLayout>
+          <Routers />
+        </MainLayout>
+        <GlobalStyle />
+      </SnackbarProvider>
     </BrowserRouter>
   );
 }
