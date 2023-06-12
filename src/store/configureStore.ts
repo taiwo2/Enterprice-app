b5300257307c4e8716f71679fa40025249d44e3a
taiwo2 <adetonat@ymail.com>
2023-06-12 23:55:10 +0100

    Refactor Reducers files

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,38 +1,63 @@
-/**
- * Create the store with dynamic reducers
- */
+// /**
+//  * Create the store with dynamic reducers
+//  */
 
-import {
-  configureStore,
-  getDefaultMiddleware,
-  StoreEnhancer,
-} from '@reduxjs/toolkit';
-import { createInjectorsEnhancer } from 'redux-injectors';
-import createSagaMiddleware from 'redux-saga';
+// import {
+//   configureStore,
+//   getDefaultMiddleware,
+//   StoreEnhancer,
+// } from '@reduxjs/toolkit';
+// import { createInjectorsEnhancer } from 'redux-injectors';
+// import createSagaMiddleware from 'redux-saga';
 
+// import { createReducer } from './reducers';
+
+// export function configureAppStore() {
+//   const reduxSagaMonitorOptions = {};
+//   const sagaMiddleware = createSagaMiddleware(reduxSagaMonitorOptions);
+//   const { run: runSaga } = sagaMiddleware;
+
+//   // Create the store with saga middleware
+//   const middlewares = [sagaMiddleware];
+
+//   const enhancers = [
+//     createInjectorsEnhancer({
+//       createReducer,
+//       runSaga,
+//     }),
+//   ] as StoreEnhancer[];
+
+//   const store = configureStore({
+//     reducer: createReducer(),
+//     middleware: [...getDefaultMiddleware(), ...middlewares],
+//     devTools: process.env.NODE_ENV !== 'production',
+//     enhancers,
+//   });
+
+//   return store;
+// }
+
+/*Create the store with dynamic reducers */
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { forceReducerReload } from 'redux-injectors';
 import { createReducer } from './reducers';
-
 export function configureAppStore() {
-  const reduxSagaMonitorOptions = {};
-  const sagaMiddleware = createSagaMiddleware(reduxSagaMonitorOptions);
-  const { run: runSaga } = sagaMiddleware;
-
-  // Create the store with saga middleware
-  const middlewares = [sagaMiddleware];
-
-  const enhancers = [
-    createInjectorsEnhancer({
-      createReducer,
-      runSaga,
-    }),
-  ] as StoreEnhancer[];
-
   const store = configureStore({
+    /*reducer is required. middleware, devTools, and the rest are optional */
     reducer: createReducer(),
-    middleware: [...getDefaultMiddleware(), ...middlewares],
+    middleware: [
+      ...getDefaultMiddleware({
+        serializableCheck: false,
+      }),
+    ],
     devTools: process.env.NODE_ENV !== 'production',
-    enhancers,
   });
-
+  /* Make reducers hot reloadable, see http://mxs.is/googmo istanbul ignore
+next */
+  if (module.hot) {
+    module.hot.accept('./reducers', () => {
+      forceReducerReload(store);
+    });
+  }
   return store;
 }
--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,21 +1,40 @@
-/**
- * Combine all reducers in this file and export the combined reducers.
- */
+// /**
+//  * Combine all reducers in this file and export the combined reducers.
+//  */
 
+// import { combineReducers } from '@reduxjs/toolkit';
+
+// import { InjectedReducersType } from 'utils/types/injector-typings';
+
+// /**
+//  * Merges the main reducer with the router state and dynamically injected reducers
+//  */
+// export function createReducer(injectedReducers: InjectedReducersType = {}) {
+//   // Initially we don't have any injectedReducers, so returning identity function to avoid the error
+//   if (Object.keys(injectedReducers).length === 0) {
+//     return state => state;
+//   } else {
+//     return combineReducers({
+//       ...injectedReducers,
+//     });
+//   }
+// }
+/* Combine all reducers in this file and export the combined reducers.
+combineReducers - turns an object whose values are different reducer
+functions into a single reducer function. */
 import { combineReducers } from '@reduxjs/toolkit';
-
-import { InjectedReducersType } from 'utils/types/injector-typings';
-
-/**
- * Merges the main reducer with the router state and dynamically injected reducers
- */
-export function createReducer(injectedReducers: InjectedReducersType = {}) {
-  // Initially we don't have any injectedReducers, so returning identity function to avoid the error
-  if (Object.keys(injectedReducers).length === 0) {
-    return state => state;
-  } else {
-    return combineReducers({
-      ...injectedReducers,
-    });
-  }
-}
+/* injectedReducers - an easier way of registering a reducer */
+const injectedReducers = {
+  //reducers here to be added one by one.
+};
+/* combineReducers requires an object.we're using the spread operator (...
+injectedReducers) to spread out all the Reducers */
+const rootReducer = combineReducers({
+  ...injectedReducers,
+});
+/* RooState is the type or shape of the combinedReducer easier way of
+getting all the types from this rootReduder instead of mapping it one
+by one. RootState - we can use the Selector to give us intelli-sense in
+building our components. */
+export type RootState = ReturnType<typeof rootReducer>;
+export const createReducer = () => rootReducer;
