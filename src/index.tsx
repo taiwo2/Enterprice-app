eb4b582070fbce223baffc6f8b028141d98e8b57
taiwo2 <adetonat@ymail.com>
2023-06-13 11:27:20 +0100

    Refactor folder

--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,8 @@ import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import 'react-quill/dist/quill.snow.css';
 import * as React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
-// Use consistent styling
 import 'sanitize.css/sanitize.css';
 
 // Import root app
@@ -22,34 +20,39 @@ import { HelmetProvider } from 'react-helmet-async';
 
 import { configureAppStore } from 'store/configureStore';
 
-import reportWebVitals from 'reportWebVitals';
-
 // Initialize languages
 import './locales/i18n';
 
 const store = configureAppStore();
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
+const MOUNT_NODE = document.getElementById('root') as HTMLElement;
 
-root.render(
+interface Props {
+  Component: typeof App;
+}
+const ConnectedApp = ({ Component }: Props) => (
   <Provider store={store}>
     <HelmetProvider>
-      {/* <React.StrictMode> */}
-        <App />
-      {/* </React.StrictMode> */}
+      <Component />
     </HelmetProvider>
-  </Provider>,
+  </Provider>
 );
+const render = (Component: typeof App) => {
+  ReactDOM.render(<ConnectedApp Component={Component} />, MOUNT_NODE);
+};
 
-// Hot reloadable translation json files
 if (module.hot) {
-  module.hot.accept(['./locales/i18n'], () => {
-    // No need to render the App again because i18next works with the hooks
+  // Hot reloadable translation json files and app
+  // modules.hot.accept does not accept dynamic dependencies,
+  // have to be constants at compile-time
+  module.hot.accept(['./app', './locales/i18n'], () => {
+    ReactDOM.unmountComponentAtNode(MOUNT_NODE);
+    const App = require('./app').App;
+    render(App);
   });
 }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(App);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
