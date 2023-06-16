4ab96e1371fe25ff6705bfe0f2a5108b41326475
taiwo2 <adetonat@ymail.com>
2023-06-16 16:40:17 +0100

    Add calendar update

---
+++ src/app/views/dashboard/CalendarView/Toolbar.tsx
@@ -0,0 +1,123 @@
+import React, { ElementType, ReactNode } from 'react';
+import clsx from 'clsx';
+import moment from 'moment';
+import {
+  Button,
+  ButtonGroup,
+  Grid,
+  // Hidden,
+  IconButton,
+  Tooltip,
+  Typography,
+  makeStyles,
+} from '@material-ui/core';
+import Hidden from '@mui/material/Hidden';
+import ViewConfigIcon from '@material-ui/icons/ViewComfyOutlined';
+import ViewWeekIcon from '@material-ui/icons/ViewWeekOutlined';
+import ViewDayIcon from '@material-ui/icons/ViewDayOutlined';
+import ViewAgendaIcon from '@material-ui/icons/ViewAgendaOutlined';
+import { ViewType } from 'models/CalendarType';
+
+type ViewOption = {
+  label: string;
+  value: ViewType;
+  icon: ElementType;
+};
+type Props = {
+  children?: ReactNode;
+  className?: string;
+  date: Date;
+  /* the ? means it's a nullable void function */
+  onDateNext?: () => void;
+  onDatePrev?: () => void;
+  onDateToday?: () => void;
+  onAddClick?: () => void;
+  /* takes a view object and returns nothing or void */
+  onViewChange?: (view: ViewType) => void;
+  view: ViewType;
+};
+const viewOptions: ViewOption[] = [
+  {
+    label: 'Month',
+    value: 'dayGridMonth',
+    icon: ViewConfigIcon,
+  },
+  {
+    label: 'Week',
+    value: 'timeGridWeek',
+    icon: ViewWeekIcon,
+  },
+  {
+    label: 'Day',
+    value: 'timeGridDay',
+    icon: ViewDayIcon,
+  },
+  {
+    label: 'Agenda',
+    value: 'listWeek',
+    icon: ViewAgendaIcon,
+  },
+];
+const Toolbar = ({
+  className,
+  date,
+  onDateNext,
+  onDatePrev,
+  onDateToday,
+  onAddClick,
+  onViewChange,
+  view,
+  ...rest
+}: Props) => {
+  const classes = useStyles();
+
+  return (
+    <Grid
+      className={clsx(classes.root, className)}
+      alignItems="center"
+      container
+      justify="space-between"
+      spacing={3}
+      {...rest}
+    >
+      <Grid item>
+        <ButtonGroup size="small">
+          <Button onClick={onDatePrev}>Prev</Button>
+          <Button onClick={onDateToday}>Today</Button>
+          <Button onClick={onDateNext}>Next</Button>
+        </ButtonGroup>
+      </Grid>
+      <Hidden smDown>
+        <Grid item>
+          <Typography variant="h3" color="textPrimary">
+            {moment(date).format('MMMM YYYY')}
+          </Typography>
+        </Grid>
+        <Grid item>
+          {viewOptions.map(viewOption => {
+            const Icon = viewOption.icon;
+            return (
+              <Tooltip key={viewOption.value} title={viewOption.label}>
+                <IconButton
+                  color={viewOption.value === view ? 'primary' : 'default'}
+                  onClick={() => {
+                    if (onViewChange) {
+                      onViewChange(viewOption.value);
+                    }
+                  }}
+                >
+                  <Icon />
+                </IconButton>
+              </Tooltip>
+            );
+          })}
+        </Grid>
+      </Hidden>
+    </Grid>
+  );
+};
+export default Toolbar;
+
+const useStyles = makeStyles(() => ({
+  root: {},
+}));
--- src/app/views/dashboard/CalendarView/index.tsx
+++ src/app/views/dashboard/CalendarView/index.tsx
@@ -1,8 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import {
   getEvents,
   openModal,
   closeModal,
+  selectRange,
+  selectEvent,
+  updateEvent,
 } from 'features/calendar/calendarSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'store/reducers';
@@ -17,14 +20,27 @@ import Page from 'app/components/pages';
 import { EventType } from 'models/CalendarType';
 import AddEditEventForm from './AddEditEventForm';
 import Header from './Header';
+import moment from 'moment';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import listPlugin from '@fullcalendar/list';
+import timelinePlugin from '@fullcalendar/timeline';
 const CalendarView = () => {
   const dispatch = useDispatch();
   const classes = useStyles();
   const { events, loading, error, isModalOpen, selectedRange } = useSelector(
     (state: RootState) => state.calendar,
   );
-
   const selectedEvent = useSelector(selectedEventSelector);
+  const mobileDevice = useMediaQuery('(max-width:600px)');
+  const [date, setDate] = useState<Date>(moment().toDate());
+  const [view, setView] = useState<ViewType>(
+    mobileDevice ? 'listWeek' : 'dayGridMonth',
+  );
+  const calendarRef = useRef<FullCalendar | null>(null);
+
   // console.log(events,'taiwo')
   useEffect(() => {
     dispatch(getEvents());
@@ -35,6 +51,19 @@ const CalendarView = () => {
   const handleModalClose = (): void => {
     dispatch(closeModal());
   };
+
+  /* calendarRef is a reference to the element FullCalendar*/
+  const handleDateNext = (): void => {
+    const calendarEl = calendarRef.current;
+    /*the getApi here is part of FullCalendar. If you 'dot space'
+the 'calendarEl,' you'll see the interfaces or APIs available. */
+    if (calendarEl) {
+      const calendarApi = calendarEl.getApi();
+      calendarApi.next();
+      setDate(calendarApi.getDate());
+    }
+  };
+  
   return (
     <Page className={classes.root} title="Calendar">
       <Container maxWidth={false}>
@@ -76,5 +105,75 @@ const useStyles = makeStyles(theme => ({
     minHeight: '100%',
     paddingTop: theme.spacing(3),
     paddingBottom: theme.spacing(3),
+  },
+  calendar: {
+    marginTop: theme.spacing(3),
+    padding: theme.spacing(2),
+    '& .fc-unthemed .fc-head': {},
+    '& .fc-unthemed .fc-body': {
+      backgroundColor: theme.palette.background.default,
+    },
+    '& .fc-unthemed .fc-row': {
+      borderColor: theme.palette.divider,
+    },
+    '& .fc-unthemed .fc-axis': {
+      ...theme.typography.body2,
+    },
+    '& .fc-unthemed .fc-divider': {
+      borderColor: theme.palette.divider,
+    },
+    '& .fc-unthemed th': {
+      borderColor: theme.palette.divider,
+    },
+    '& .fc-unthemed td': {
+      borderColor: theme.palette.divider,
+    },
+    '& .fc-unthemed td.fc-today': {},
+    '& .fc-unthemed .fc-highlight': {},
+    '& .fc-unthemed .fc-event': {
+      backgroundColor: theme.palette.secondary.main,
+      color: theme.palette.secondary.contrastText,
+      borderWidth: 2,
+      opacity: 0.9,
+      '& .fc-time': {
+        ...theme.typography.h6,
+        color: 'inherit',
+      },
+      '& .fc-title': {
+        ...theme.typography.body1,
+        color: 'inherit',
+      },
+    },
+    '& .fc-unthemed .fc-day-top': {
+      ...theme.typography.body2,
+    },
+    '& .fc-unthemed .fc-day-header': {
+      ...theme.typography.subtitle2,
+      fontWeight: theme.typography.fontWeightMedium,
+      color: theme.palette.text.secondary,
+      padding: theme.spacing(1),
+    },
+    '& .fc-unthemed .fc-list-view': {
+      borderColor: theme.palette.divider,
+    },
+    '& .fc-unthemed .fc-list-empty': {
+      ...theme.typography.subtitle1,
+    },
+    '& .fc-unthemed .fc-list-heading td': {
+      borderColor: theme.palette.divider,
+    },
+    '& .fc-unthemed .fc-list-heading-main': {
+      ...theme.typography.h6,
+    },
+    '& .fc-unthemed .fc-list-heading-alt': {
+      ...theme.typography.h6,
+    },
+    '& .fc-unthemed .fc-list-item:hover td': {},
+    '& .fc-unthemed .fc-list-item-title': {
+      ...theme.typography.body1,
+    },
+    '& .fc-unthemed .fc-list-item-time': {
+      ...theme.typography.body2,
+    },
   },
 }));
