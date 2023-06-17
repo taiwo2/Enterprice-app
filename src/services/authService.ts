812f111c30e0801570660e643bbf29d6a6902861
taiwo2 <adetonat@ymail.com>
2023-06-18 00:01:47 +0100

    Add Login Form

---
+++ src/services/authService.ts
@@ -0,0 +1,25 @@
+import axios, { EndPoints } from 'api/axios';
+
+export type UserModel = {
+  email: string;
+  password: string;
+};
+/*The return object will be an object with an access token of type string.
+ We're expecting an access token from the json-server-auth */
+export async function loginAxios(userModel: UserModel) {
+  return await axios.post<{ accessToken: string }>(EndPoints.login, userModel);
+}
+
+export type RegisterModel = {
+  email: string;
+  password: string;
+  name: string;
+  mobile: string;
+  policy: boolean;
+};
+export async function registerAxios(registerModel: RegisterModel) {
+  return await axios.post<{ accessToken: string }>(
+    EndPoints.register,
+    registerModel,
+  );
+}
