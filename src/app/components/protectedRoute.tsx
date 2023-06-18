c722d29f0a157d202f87002ad5348cb337de6621
taiwo2 <adetonat@ymail.com>
2023-06-18 01:03:08 +0100

    Update protected route

--- src/app/components/protectedRoute.tsx
+++ src/app/components/protectedRoute.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import { Navigate, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import jwt_decode from 'jwt-decode';
+import { saveClaimsAction } from 'features/auth/authSlice';
+import { ClaimsType } from 'models/claimsType';
 const ProtectedRoute = props => {
+  const dispatch = useDispatch();
   const token = localStorage.getItem('token');
-  return token ? (
+  /* this is cleaning up the localStorage and redirecting user to login */
+  if (!token) {
+    localStorage.clear();
+    return <Navigate to={{ pathname: '/login' }} />;
+  }
+  const decoded: ClaimsType = jwt_decode(token);
+  const expiresAt = decoded.exp * 1000;
+  const dateNow = Date.now();
+  const isValid = dateNow <= expiresAt;
+  dispatch(saveClaimsAction(decoded));
+  return isValid ? (
     <Route {...props} />
   ) : (
     <Navigate to={{ pathname: '/login' }} />
